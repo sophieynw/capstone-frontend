c6fe76662611291efa47f075137ec4393817fac5
Sophie Wang <wang.sophie@live.ca>
2026-07-20 23:08:02 -0400

    Revert "Revert "Navigation & Safe Area Provider""

---
+++ restfulhousekeeping/screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+      <Text>Welcome to the Home screen!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
